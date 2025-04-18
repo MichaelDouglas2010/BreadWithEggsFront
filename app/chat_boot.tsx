edc728fc748142529ca34315b445b09b774cb6cf
MichaelDouglas2010 <michaeldouglasramos2010@gmail.com>
2025-04-18 16:29:43 -0300

    chore: Add bot in the application

---
+++ app/chat_boot.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+const ChatScreen = () => {
+  return (
+    <View style={styles.container}>
+      <WebView
+        source={{ uri: "https://cdn.botpress.cloud/webchat/v2.3/shareable.html?configUrl=https://files.bpcontent.cloud/2025/04/18/16/20250418163616-UERMLA86.json" }}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        style={styles.webview}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  webview: {
+    flex: 1,
+  },
+});
+
+export default ChatScreen;
