4e66486b316058a34965989ee3fc7a3c8513c7de
MichaelDouglas2010 <michaeldouglasramos2010@gmail.com>
2025-06-21 20:48:15 -0300

    feat: merge changes of sdk and screens

--- components/tabelas/Equipment_table_delete.tsx
+++ components/tabelas/Equipment_table_delete.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import { Text } from 'react-native';
 import GenericTable from './GenericTable';
 import { EquipmentGet } from '../interfaces/equipment';
